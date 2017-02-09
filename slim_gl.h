a41b1858f5757d4c97ee19beb592687180e331f2
Stephan Soller <stephan.soller@helionweb.de>
2017-02-09 22:29:13 +0100

    Added first version of the Slim GL library.

---
+++ slim_gl.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+
+
+GLuint program_new(const char* vertex_shader_file, const char* fragment_shader_file, FILE* compiler_message_stream);
+GLuint program_new_from_string(const char* vertex_shader_code, const char* fragment_shader_code, FILE* compiler_message_stream);
+void   program_destroy(GLuint program);
+void   program_inspect(GLuint program, FILE* output_stream);
+
+GLuint buffer_new(const void* data, size_t size);
+void   buffer_destroy(GLuint buffer);
+void   buffer_update(GLuint buffer, const void* data, size_t size, GLenum usage);
+
+GLuint texture_new(const void* data, size_t width, size_t height, uint8_t components);
+GLuint texture_new_rect(const void* data, size_t width, size_t height, uint8_t components);
+void   texture_destroy(GLuint texture);
+void   texture_update(GLuint texture, const void* data);
+
+GLuint framebuffer_new(GLuint color_buffer_texture);
+void   framebuffer_destroy(GLuint framebuffer);
+void   framebuffer_blit(GLuint read_framebuffer, GLint rx, GLint ry, GLint rw, GLint rh, GLuint draw_framebuffer, GLint dx, GLint dy, GLint dw, GLint dh);
+void   framebuffer_bind(GLuint framebuffer);
+
+int    render(GLenum primitive, GLuint program, const char* bindings, ...);
+
+#ifdef _SGL_UTILS
+void* fload(const char* filename, size_t* size);
+#endif
